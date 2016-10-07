d640bd9c585c2dd7909cd04eab0ec554fb91fe92
cindy <cindywmiao@gmail.com>
2016-10-07 10:18:36 +0800

    add misc.scala

--- src/exercises/WorkingWithLists.scala
+++ src/exercises/WorkingWithLists.scala
@@ -1,26 +1,59 @@
 object WorkingWithLists{
 
 	/*Find the last element of a list.*/
-	def last(list : List[Int]) : Int = {
-		list.last
-	}
+	// There are several ways to solve this problem.  If we use builtins, it's very easy.
+  	def lastBuiltin[A](ls: List[A]): A = ls.last
+
+  	// The standard functional approach is to recurse down the list until we hit
+  	// the end.  Scala's pattern matching makes this easy.
+  	def lastRecursive[A](ls: List[A]): A = ls match {
+    	case h :: Nil  => h
+    	case _ :: tail => lastRecursive(tail)
+    	case _         => throw new NoSuchElementException
+  	}
 
 	/*Find the last but one element of a list.*/
-	def penultimate(list : List[Int]) : Int = {
-		if(list == Nil || list.length <= 1)
-			0
-		else
-			list(list.length - 2)
+	def penultimateBuiltin[A](ls: List[A]): A =
+    	if (ls.isEmpty) throw new NoSuchElementException
+    	else ls.init.last
+
+	def penultimateRecursive[A](ls : List[A]) : A = ls match {
+		case h :: _ :: Nil => h
+    	case _ :: tail     => penultimateRecursive(tail)
+    	case _             => throw new NoSuchElementException
 	}
+	def lastNthBuiltin[A](n: Int, ls: List[A]): A = {
+    	if (n <= 0) throw new IllegalArgumentException
+    	if (ls.length < n) throw new NoSuchElementException
+    	ls.takeRight(n).head
+  	}
+
+	def lastNthRecursive[A](n: Int, ls: List[A]): A = {
+    	def lastNthR(count: Int, resultList: List[A], curList: List[A]): A = curList match {
+        	case Nil if count > 0 => throw new NoSuchElementException
+        	case Nil              => resultList.head
+        	case _ :: tail        =>
+          		lastNthR(count - 1,
+                   if (count > 0) resultList else resultList.tail,
+                   tail)
+      	}
+    	if (n <= 0) throw new IllegalArgumentException
+    	else lastNthR(n, ls, ls)
+  	}
 
 	/*Find the Kth element of a list.
 	By convention, the first element in the list is element 0.*/
-	def nth(index : Int, list : List[Int]) : Int = {
-		if(list == Nil || list.length == 0 || list.length < index) 
-			0
-		else 
-			list(index)
-	}
+	// Trivial with builtins.
+  	def nthBuiltin[A](n: Int, ls: List[A]): A =
+    	if (n >= 0) ls(n)
+    	else throw new NoSuchElementException
+
+  	// Not that much harder without.
+  	def nthRecursive[A](n: Int, ls: List[A]): A = (n, ls) match {
+    	case (0, h :: _   ) => h
+    	case (n, _ :: tail) => nthRecursive(n - 1, tail)
+    	case (_, Nil      ) => throw new NoSuchElementException
+  	}
 
 	/*Find the number of elements of a list.*/
 	def length(list: List[Int]) : Int = {
@@ -52,7 +85,7 @@ object WorkingWithLists{
 
 	def main(args: Array[String]): Unit = {
 		val list =  List(1, 1, 2, 3, 5, 8)
-		println(last(list))
+		println(lastBuiltin(list))
 		println(penultimate(list))
 		println(nth(2, list))
 		println(length(list))
